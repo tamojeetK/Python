6ac32e65e9107e586b7ef43f5e2be5053436346b
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-04 00:22:33 +0530

    Add files via upload

---
+++ 1main.py
@@ -0,0 +1,20 @@
+'''
+This is a demo for 
+printing 
+in Python
+'''
+
+print(" Hello World!!" , "Namaste Duniya !!")
+
+# **********
+x=33
+y=53
+sum=x+y
+print("Hey the sum of x and y is " , sum)
+
+"""
+Sab
+Moh 
+Maya 
+Hai
+"""
