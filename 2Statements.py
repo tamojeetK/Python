4b91d670e59b792cad2a4e18dd1fabeef1d2491e
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-04 12:08:34 +0530

    Add files via upload

---
+++ 2Statements.py
@@ -0,0 +1,23 @@
+# Example 1
+#***************
+
+x = 4
+y = 3
+print("The Sum of x and y is = " , x + y)
+
+#*****************************************************
+
+# Multiple Statements in One Line(use of ;)
+#***********************************
+
+a = 5; b = 6; print("The Sum of a and b is =" , a + b)
+
+#****************************************************
+
+# One statement in Multiple Lines
+#*********************************
+fruits = ["orange",
+        "mango",
+        "banana"]
+
+print (fruits)
