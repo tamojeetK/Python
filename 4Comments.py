a122598a9223785f66f24b3d9e230d4203470f91
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-05 12:01:59 +0530

    Add files via upload

---
+++ 4Comments.py
@@ -0,0 +1,19 @@
+# this is a comment
+x = 3
+y = 4
+print(x + y)
+# add x and y
+
+# ************************
+
+# Single line Comment
+print("\nHello World!")
+
+# ************************
+
+# Multiple Line Comment
+# comment here
+# comment here
+# comment here
+# comment here
+print("\nByee !!")
