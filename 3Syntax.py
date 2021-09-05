0a021b1e1578df882a626eed48a7a769a1a42a50
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-05 12:01:40 +0530

    Add files via upload

---
+++ 3Syntax.py
@@ -0,0 +1,23 @@
+#Indentation 
+#****************
+
+# if 5 > 4:
+#    x = "Hello World !!"
+#    print(x)
+
+ ########################  
+
+#Ending a block of statements:-
+#*********************
+
+if 5 > 4:
+   x = "Hello World !!"
+   print(x)
+print("The block of statement below end here.")
+#***************
+# if the statement is false then "Hello World !!" won't be printed.
+#here 3 is > 4 so statement will not be printed.
+if 3 > 4:
+   x = "Hello World !!"
+   print(x)
+print("\nThe block of statement below end here.")
