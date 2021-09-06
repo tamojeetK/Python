9ed0eaa08817c6a2db012e5a378d9710e45f410f
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-06 08:37:25 +0530

    Add files via upload

---
+++ 5Variables.py
@@ -0,0 +1,17 @@
+# name = "Juan"
+# print("Hello " + name)
+
+# CHANGING A VARIABLE VALUE
+# *****************************
+# name = "Juan"
+# name = "John"
+# print("Hello " + name)
+    # [We can aso assign a variable to a value of another type]
+# name = "Juan"
+# name = 21
+# print(name)
+
+# LONG VARIABLE NAMES
+# ******************************
+my_favourite_fruit = "mango"
+print(my_favourite_fruit)
