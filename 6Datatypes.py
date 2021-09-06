d6cfc04f311426138b1c7233cf389c548eddf4d7
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-06 08:37:44 +0530

    Add files via upload

---
+++ 6Datatypes.py
@@ -0,0 +1,46 @@
+# NUMBERS
+
+# x is a int
+x = 21
+print("The value of x is : "  , x)
+
+#*
+a = 5
+b = 6
+print("The sum of a and b is : " , a + b)
+
+#***********************
+# STRINGS
+
+fruit = 'mango' #using single quote
+print(fruit)
+fruit = "mango" #using double quote
+print(fruit)
+
+y = 'I told u "python is fun to learn.'
+print(y)
+
+#***********************
+# BOOLEANS
+print(10 > 5)
+print(6 > 3)
+print(4 < 2)
+print(5 == 5)
+print(5 == 6)
+
+
+#************************
+
+# LISTS
+mylist1 = ["mango" , "orange" , "apple" , "cherry"]
+print(mylist1)
+my_list = [2, 3, 4, 5, 6]
+print(my_list)
+
+
+
+
+
+# print("Enter a number : ")
+# x = input()
+# print("The number is : ", int(x) )
