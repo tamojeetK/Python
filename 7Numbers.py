5d151e464642e6af45517acf4caa109b66da2156
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-06 08:38:02 +0530

    Add files via upload

---
+++ 7Numbers.py
@@ -0,0 +1,27 @@
+# GETTING TYPE OF NUMERICAL VALUES
+a = 3
+b = 21
+c = 4.0
+d = 5.21
+e = 4.21j
+f = 5.33j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+# *******************************
+
+# APPLYING ARITHMATIC OPERATIONS
+w = 4+2
+print("The value of x is :", w)
+
+x = 4-2
+print("The value of x is :", x)
+
+y = 4*2
+print("The value of y is :", y)
+
+z = 4/2
+print("The value of z is :", z)
