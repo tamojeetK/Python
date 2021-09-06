9cc1f10cabd86943d97913c182ea873d1a94c36d
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-06 08:38:21 +0530

    Add files via upload

---
+++ 8NumberMethods.py
@@ -0,0 +1,26 @@
+# ROUNDING A NUMBER >> round(number, ndigits)
+pi = 3.1415926535897
+two_decimals = round(pi, 2)
+three_decimals = round(pi, 3)
+
+print(two_decimals)
+print(three_decimals)
+# *************************
+
+# RAISING A NUMBER TO A POWER >> pow(base, exp)
+x = pow(2, 2)
+y = pow(4, 3)
+print(x)
+print(y)
+# *************************
+
+# GETTING ABSOLUTE VALUE 
+a = abs(-4)
+b = abs(-3.21)
+print(a)
+print(b)
+# *************************
+
+# GETTING QUOTIENT AND REMINDER
+z = divmod(5, 2)
+print(z)
