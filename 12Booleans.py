d2d834a7c6c24eccaeb22a8d1b0e86d6e666ca67
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-09 19:02:31 +0530

    Add files via upload

---
+++ 12Booleans.py
@@ -0,0 +1,18 @@
+a = True
+b = False
+c = "ZoopZoop" #TEST ignore
+print(a)
+print(b)
+print(c)
+# ***********
+
+print(5 == 5)
+print(10 > 5)
+print(20 < 10)
+# ***********
+
+x = type(a)
+print(x)
+
+y = type(c) #TEST ignore
+print(y)
