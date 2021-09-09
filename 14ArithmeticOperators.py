75470926ad463475d7740bed1d8636d30125e3e2
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-09 19:03:01 +0530

    Add files via upload

---
+++ 14ArithmeticOperators.py
@@ -0,0 +1,31 @@
+'''
++  Addition
+
+-  Subtraction
+
+*  Multiplication
+
+/  Division
+
+** Exponentiation
+
+%  Reminder
+
+// Floor Division 
+
+'''
+x = 10
+
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y) #note : quotient is always floating pt. no.
+print( 2 ** 3)
+print(5 % 2)
+print(5 // 2)
+
+# --Operator Sequence--
+#  B.O.D.M.A.S (** > [* / % //] > [+ -] > [== != <= = < >])
+#use () if certain operations are needed to be performed first
