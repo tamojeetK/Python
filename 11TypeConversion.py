3f43356c51531bf33cb44dc2c064b36507d1eb8d
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-09 19:01:55 +0530

    Add files via upload

---
+++ 11TypeConversion.py
@@ -0,0 +1,42 @@
+# Convert to String 
+x = 21
+x = str(x)
+text = "\nMy favourite number is " + x
+print(text)
+
+        # or 
+       # just (≧▽≦) 
+       
+x1 = 22
+print("My favourite number is" , x1, "\n")
+
+# done xD 
+#  ********************
+
+# Convert to Integer
+x2 = 4.55
+x2 = int(x2)
+print("x2 = ", x2)
+
+    # or in oneline code :
+x3 = 4.66
+print("The Normal form =", x3, "\nThe Converted form =", int(x3), "\n")
+
+# Sum 
+num1 = "40"
+sum = int(num1) + 60
+print("Sum is: ", sum, "\n")
+
+# *********************
+
+# Convert to Float 
+x4 = 4
+x5 = float(x4)
+
+print(x4)
+print(x5)
+
+# Sum 
+num2 = "4.3"
+sum = float(num2) - 6.5
+print(sum)
