b0ff6dee7e903f0b0a8dc0bc223e56a6bd991aa5
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-13 09:20:59 +0530

    Add files via upload

---
+++ 16ComparisionOperator.py
@@ -0,0 +1,28 @@
+# Equality ==
+print (5 == 5)
+print (2 == 3)
+x = "python" == "python"
+print (x, "\n")
+
+# Inequality !=
+print(10 != 7)
+y = "python" != "javascript"
+print(y, "\n")
+
+# Greater Than >
+print(8 > 4)
+print(5 > 10, "\n")
+
+# Less Than <
+print(10 < 15)
+print(20 < 10, "\n")
+
+# Greater than or Equal to >=
+print(8 >= 4)
+print(8 >= 8)
+print(5 >= 10, "\n")
+
+# Less than or Equal to <=
+print(10 <= 15)
+print(10 <= 10)
+print(20 <= 10)
