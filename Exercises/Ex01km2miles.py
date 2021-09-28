abd385a0fde8067d1d9d39b20ad08d8079c2ddba
Tamojeet Kuila (Ahaan) <89387048+tamojeetK@users.noreply.github.com>
2021-09-28 23:47:11 +0530

    Create Ex01km2miles.py

---
+++ Exercises/Ex01km2miles.py
@@ -0,0 +1,7 @@
+print("\n------This program Converts Km to Miles-----")
+# 1 mile = 1.609344 km 
+km = float(input("Enter the value in Km: "))
+miles = km / 1.609344
+print(km, "km = ",miles , "miles")
+print('\nor\n\n',km, "km = ", round(miles, 2), "miles")
+
